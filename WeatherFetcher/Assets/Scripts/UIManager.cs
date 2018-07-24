85c42522e85b614f7bb99da5d3dba2231abb6104
Anas Bitar <anas.173@hotmail.com>
2018-07-24 15:23:59 +0400

    project

---
+++ WeatherFetcher/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager uiManager;
+
+    public Text countryName;
+    public Text countryTemperature;
+    public Text countryLatitude, countryLongitude;
+    public Text countryCode;
+    public Text countrySunrise;
+    public Text countrySunset;
+    public Text countryWeatherDescription;
+    public Text countryHumidity;
+
+    void Awake()
+    {
+        uiManager = this;
+    }
+    
+}
